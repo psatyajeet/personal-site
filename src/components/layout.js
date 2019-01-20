1f726731054b1a1d7eee98c3abbc445e476ee279
Satyajeet Pal <psatyajeet@gmail.com>
2019-01-20 10:31:13 -0800

    Include styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,44 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
-import Header from './header'
-import './layout.css'
+const Container = styled.div`
+  margin: 0px auto;
+  max-width: 1050px;
+  padding: 0px 1rem;
+`
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default ({ children }) => <Container>{children}</Container>
