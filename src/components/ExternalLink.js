e5d92315d6d35038f446cb918dba31d8967a6916
Satyajeet Pal <psatyajeet@gmail.com>
2019-01-20 22:00:38 -0800

    Fix duplicate links

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 export default ({ className, to, children }) => (
-  <a
-    className={className}
-    target="_blank"
-    rel="noopener noreferrer"
-    href="https://www.bitwiseinvestments.com/"
-    href={to}
-  >
+  <a className={className} target="_blank" rel="noopener noreferrer" href={to}>
     {children}
   </a>
 )
