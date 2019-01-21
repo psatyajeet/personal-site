663daf22746524aefe57e756f76345d7d9f23a53
Satyajeet Pal <psatyajeet@gmail.com>
2019-01-20 18:34:15 -0800

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,6 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
@@ -45,6 +44,6 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
